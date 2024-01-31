baff803817a9f45b03bb8486888f870b405a17fb
hosshinon <hosshinon@gmail.com>
2024-01-31 20:05:18 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -46,62 +46,66 @@ function App() {
   const handleCheckd = (id: number, checked: boolean) => {
     const newTodos = todos.map((todo) => {
       if (todo.id === id) {
-        todo.checked=!checked
+        todo.checked = !checked;
       }
-      return todo
-    })
-    setTodos(newTodos)
-  }
-  
-  const handleDelete = (id: number) => { 
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleDelete = (id: number) => {
     const newTodos = todos.filter((todo) => {
-      return todo.id !== id
-    } )
-    setTodos(newTodos)
-   }
+      return todo.id !== id;
+    });
+    setTodos(newTodos);
+  };
 
   return (
     <div className='App'>
-      <div className=' '>
-        <h2 className='bg-green-200 text-xl'>Todoリスト with TypeScript</h2>
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-        >
-          <input
-            className='border-2  '
-            type='text'
-            onChange={(e) => {
-              handleChange(e);
-            }}
-          />
-          <input
-            type='submit'
-            value='作成'
-            className='border-2'
-            onChange={() => {}}
-          />
-        </form>
 
-        <ul>
-          {todos.map((todos) => (
-            <li key={todos.id}>
-              <input
-                className='border-2  '
-                type='text'
-                onChange={(e) => {
-                  handleEdit(todos.id, e.target.value);
-                }}
-                value={todos.inputValue}
-                disabled={todos.checked}
-              />
-              <input type='checkbox'onChange={(e) => {handleCheckd(todos.id, todos.checked);}}/>
-              <button onClick={()=>handleDelete(todos.id)}>削除</button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <h2 className='bg-green-200 text-xl'>Todoリスト with TypeScript</h2>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          className='border-2  '
+          type='text'
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <input
+          type='submit'
+          value='作成'
+          className='border-2'
+          onChange={() => {}}
+        />
+      </form>
+
+      <ul>
+        {todos.map((todos) => (
+          <li key={todos.id}>
+            <input
+              className='border-2  '
+              type='text'
+              onChange={(e) => {
+                handleEdit(todos.id, e.target.value);
+              }}
+              value={todos.inputValue}
+              disabled={todos.checked}
+            />
+            <input
+              type='checkbox'
+              onChange={(e) => {
+                handleCheckd(todos.id, todos.checked);
+              }}
+            />
+            <button onClick={() => handleDelete(todos.id)}>削除</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
